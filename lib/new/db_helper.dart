d02659270af5d2f4971776fb2e7ab47acb6fc0ff
WeerachaiKojakung <63020821@up.ac.th>
2023-09-22 16:04:38 +0700

    first commit

--- lib/new/db_helper.dart
+++ lib/new/db_helper.dart
@@ -3,24 +3,39 @@ import 'package:sqflite/sqflite.dart' as sql;
 
 class SQLHelper {
   static Future<void> createTables(sql.Database database) async {
-    await database.execute("""CREATE TABLE data (
-      id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+  await database.execute("""CREATE TABLE data (
+    id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
       title TEXT,
       desc TEXT,
+      aa TEXT,
+      bb TEXT,
       createdAt TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP)""");
   }
 
   static Future<sql.Database> db() async {
-    return sql.openDatabase("database_name.db", version: 1,
-        onCreate: (sql.Database database, int version) async {
+    return sql.openDatabase(
+      "database_name.db", 
+      version: 3, // เพิ่มเวอร์ชันของฐานข้อมูล
+     onCreate: (sql.Database database, int version) async {
       await createTables(database);
-    });
+    },
+    );
   }
 
-  static Future<int> createData(String title, String? desc) async {
+  static Future<int> createData(
+    String title,
+    String? desc, // พารามิเตอร์ที่เป็นตัวเลือก
+    String? aa, // พารามิเตอร์ที่เป็นตัวเลือก
+    String? bb, // พารามิเตอร์ที่เป็นตัวเลือก
+  ) async {
     final db = await SQLHelper.db();
 
-    final data = {'title': title, 'desc': desc};
+    final data = {
+      'title': title,
+      'desc': desc,
+      'aa': aa, // รวม 'aa' ในฟิลด์
+      'bb': bb, // รวม 'bb' ในฟิลด์
+    };
     final id = await db.insert('data', data,
         conflictAlgorithm: sql.ConflictAlgorithm.replace);
 
@@ -37,11 +52,19 @@ class SQLHelper {
     return db.query('data', where: "id = ?", whereArgs: [id], limit: 1);
   }
 
-  static Future<int> updateData(int id, String title, String? desc) async {
+  static Future<int> updateData(
+    int id,
+    String title,
+    String? desc, // พารามิเตอร์ที่เป็นตัวเลือก
+    String? aa, // พารามิเตอร์ที่เป็นตัวเลือก
+    String? bb, // พารามิเตอร์ที่เป็นตัวเลือก
+  ) async {
     final db = await SQLHelper.db();
     final data = {
       'title': title,
       'desc': desc,
+      'aa': aa, // รวม 'aa' ในฟิลด์
+      'bb': bb, // รวม 'bb' ในฟิลด์
       'createdAt': DateTime.now().toString()
     };
     final result =
@@ -53,6 +76,6 @@ class SQLHelper {
     final db = await SQLHelper.db();
     try {
       await db.delete('data', where: "id = ?", whereArgs: [id]);
-    }catch (e) {}
+    } catch (e) {}
   }
 }
